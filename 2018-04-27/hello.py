7d05c61760339a84e7e5d30d50f3e2cb19572665
Your Name <yueyuezhu@msn.com>
2018-05-04 22:37:08 +0800

    2018-05-04

---
+++ 2018-04-27/hello.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/user/<name>')
+def user(name):
+    return render_template('user.html', name=name)
+
+
+@app.route('/test/<comments>')
+def test(comment):
+    return render_template('test.html', comment=comment)
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
+
+if __name__ == '__main__':
+    app.run()
