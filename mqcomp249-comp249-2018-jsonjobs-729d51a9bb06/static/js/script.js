80804c2625ba3a09546bc958f8425ae1b2feb750
Your Name <yueyuezhu@msn.com>
2018-06-06 19:50:12 +0800

    2018-06-06

--- mqcomp249-comp249-2018-jsonjobs-729d51a9bb06/static/js/script.js
+++
@@ -1,8 +0,0 @@
-(function(){
-
-    /* your code goes here */
-
-    $(document).ready(function(){
-        $("#content").text("This text was placed here by the javascript in static/js/script.js")
-        })
-})()
